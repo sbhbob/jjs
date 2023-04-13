82cc23130c1626228752ac32bcea59bd2696cb20
Skyler Hope <99628593+sbhbob@users.noreply.github.com>
2023-04-13 08:53:28 -0600

    first commit

---
+++ GroupCapstone/GroupCapstone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupCapstone
+//
+//  Created by Skyler Hope on 4/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
