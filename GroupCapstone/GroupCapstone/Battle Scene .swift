8b7ff7e712886936c32d5ff600a46c914752f2a2
Junnesm <junne.murdock@gmail.com>
2023-04-24 08:03:39 -0600

    images

--- GroupCapstone/GroupCapstone/Battle Scene .swift
+++ GroupCapstone/GroupCapstone/Battle Scene .swift
@@ -6,8 +6,29 @@
 //
 
 import Foundation
+import UIKit
 
 class BattleSceneViewController {
+    
+    // firecave, ocean, stormySky, volcano, mountain, plains, parkingGarage, timesSquare, graveyard, outerSpace, yourMomsHouse, CERN, swamp, sewer
+    
+    
+    @IBOutlet weak var FireCaveElement: UIImageView!
+    @IBOutlet weak var OceanElement: UIImageView!
+    @IBOutlet weak var StormySkyElement: UIImageView!
+    @IBOutlet weak var VolcanoElement: UIImageView!
+    @IBOutlet weak var MountainElement: UIImageView!
+    @IBOutlet weak var PlainsElement: UIImageView!
+    @IBOutlet weak var ParkingGarageElement: UIImageView!
+    @IBOutlet weak var TimesSquareElement: UIImageView!
+    @IBOutlet weak var GraveYardElement: UIImageView!
+    @IBOutlet weak var OuterSpaceElement: UIImageView!
+    @IBOutlet weak var YourMomsHouseElement: UIImageView!
+    @IBOutlet weak var CernElement: UIImageView!
+    @IBOutlet weak var SwampElement: UIImageView!
+    @IBOutlet weak var SewerElement: UIImageView!
+    
+    
     
     
 }
